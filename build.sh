914b8a2e1a714386840c9cfa5a432b073606a42b
hrishabht5 <74245064+hrishabht5@users.noreply.github.com>
2025-05-31 22:26:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y firefox-esr wget
-wget https://github.com/mozilla/geckodriver/releases/latest/download/geckodriver-linux64.tar.gz
-tar -xvzf geckodriver-linux64.tar.gz
+
+# Install Firefox (headless)
+apt-get update && apt-get install -y firefox-esr wget bzip2
+
+# Download a known working version of geckodriver
+GECKO_VERSION=v0.34.0
+wget https://github.com/mozilla/geckodriver/releases/download/$GECKO_VERSION/geckodriver-$GECKO_VERSION-linux64.tar.gz
+
+# Extract and move to a known location
+tar -xvzf geckodriver-$GECKO_VERSION-linux64.tar.gz
 mv geckodriver /usr/bin/geckodriver
+chmod +x /usr/bin/geckodriver
+
+# Clean up
+rm geckodriver-$GECKO_VERSION-linux64.tar.gz
